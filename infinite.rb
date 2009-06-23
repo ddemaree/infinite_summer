76df8a87123102a7e0377d491a68fd6b2e14b442
David Demaree <david@practical.cc>
2009-06-23 10:55:08 -0500

    Initial commit

---
+++ infinite.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'sinatra'
+
+require 'activesupport'
+require 'fastercsv'
+
+schedule = FCSV.read "./schedule.csv", :headers => true, :header_converters => :symbol, :converters => [:date, :numeric, (lambda { |f| ( f =~ /^\d+\// ? Date.parse(f) : f) })]
+
+current_row = nil
+
+schedule.each do |row|
+  current_row = row
+  break if row[:date] > Date.today
+end
+
+get '/' do
+  # Get indiv vars for params
+  @milestone, @page, @location = current_row[:date], current_row[:page], current_row[:location]
+  erb :index
+end
